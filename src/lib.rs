64e5e6e5e4202457f9f0789c1f0524c6a7c0aca2
jasperdg <jasperdegooijer@gmail.com>
2021-10-09 13:18:47 +0100

    expose pub structures by declaring use as pub

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,23 @@ pub mod consts;
 use consts::*;
 
 pub mod types;
-use types::*;
+pub use types::*;
 
 pub mod nonce;
-use nonce::*;
+pub use nonce::*;
 
 pub mod outcome;
-use outcome::*;
+pub use outcome::*;
 
 pub mod requester;
-use requester::*;
+pub use requester::*;
 
 pub mod data_request;
-use data_request::*;
+pub use data_request::*;
 
 pub mod resolution_window;
-use resolution_window::*;
+pub use resolution_window::*;
 
 pub mod config;
-use config::*;
+pub use config::*;
 
